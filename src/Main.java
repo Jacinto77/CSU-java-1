1fc1b2c4b17cace0c346bca5447c2772cce33c1a
Max Holt <max.holt1@gmail.com>
2024-08-17 10:40:56 -0400

    week 1 homework

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+    public static void main(String[] args) {
+        String firstName, lastName, address, city;
+        int zip;
+        firstName = "Captain";
+        lastName = "Marvel";
+        address = "6321 Laurel Canyon Boulevard";
+        city = "North Hollywood";
+        zip = 91601;
+
+        System.out.println(firstName + " " + lastName);
+        // System.out.println(lastName);
+        System.out.println(address + ",");
+        System.out.println(city + ",");
+        System.out.println(zip);
+    }
+}
